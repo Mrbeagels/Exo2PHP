494fc47f204daf62392a35a53c58903d1db00bca
Thibaud Marin <thibaud.marin1@gmail.com>
2022-06-08 11:45:07 +0200

    premier commit

---
+++ exo1/index.php
@@ -0,0 +1,8 @@
+<p class="titre"> Exo2. Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix.  Attention age est de type entier.  Afficher leur contenu. </p>
+
+<p class="exo"<?php
+$lastname = 'bob';
+$firstname = 'vance';
+$age = 28;
+echo "> $lastname, $firstname, $age "
+?></p>
